4befedb70d97d0b1caa1e681c5476e2dae85602f
Barney Dono <sovietspy2@users.noreply.github.com>
2023-09-05 16:52:34 +0200

    adding JWT cookie auth

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token,get_jwt_identity
+from flask import Flask, jsonify, request,make_response
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token,get_jwt_identity, set_access_cookies
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'your-secret-key'
 jwt = JWTManager(app)
+CORS(app, supports_credentials=True, origins=['http://localhost:1234'])
+app.config["JWT_COOKIE_SECURE"] = False
+app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies", "json", "query_string"]
+
 
 registered_users = []
 
@@ -34,13 +39,18 @@ def login():
 
     # Create and return an access token
     access_token = create_access_token(identity=username)
-    return jsonify({'access_token': access_token}), 200
+    response = make_response(jsonify({'message': 'Login successful'}), 200)
+    set_access_cookies(response, access_token)
+    
+    return response
 
-@app.route('/protected', methods=['GET'])
-@jwt_required()
+@app.route('/protected', methods=['POST'])
+@jwt_required(locations=["cookies"])
 def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
