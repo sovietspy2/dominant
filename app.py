805629fae1ad9c1cf8ac6cbdcdbd2adab8c769e2
Barney Dono <sovietspy2@users.noreply.github.com>
2023-09-08 20:12:45 +0200

    adding token refresh

--- app.py
+++ app.py
@@ -3,18 +3,17 @@ from flask_jwt_extended import JWTManager, jwt_required, create_access_token,get
 from flask_cors import CORS
 import sqlite3
 import bcrypt
+from datetime import timedelta
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'your-secret-key'
-jwt = JWTManager(app)
-CORS(app, supports_credentials=True, origins=['http://localhost:1234'])
 app.config["JWT_COOKIE_SECURE"] = False
 app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies", "json", "query_string"]
+app.config["JWT_EXPIRATION_DELTA"] = timedelta(hours=12)
+jwt = JWTManager(app)
+CORS(app, supports_credentials=True, origins=['http://localhost:1234'])
 
 connection = sqlite3.connect("database.db")
-
-
-registered_users = []
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -66,7 +65,7 @@ def login():
     return jsonify({"message": "Invalid credentials"}), 400
 
 @app.route('/protected', methods=['POST'])
-@jwt_required(locations=["cookies"])
+@jwt_required()
 def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
@@ -80,12 +79,40 @@ def user():
 
 # Logout route
 @app.route("/logout", methods=["POST"])
-@jwt_required()
 def logout():
     # In this example, we don't refresh the token, effectively "logging out"
     resp = jsonify({"message": "Logout successful"})
     unset_jwt_cookies(resp)
     return resp, 200
+
+# Custom @app.after_request handler
+@app.after_request
+def refresh_expired_token(response):
+    try:
+        # Check if the current request had a valid JWT token
+        identity = jwt.get_jwt_identity()
+
+        if identity:
+            # Calculate the remaining time until token expiration
+            expires = jwt.get_expiration()
+            now = jwt.get_iat()
+            remaining_time = expires - now
+
+            # Define a threshold for refreshing the token (e.g., refresh 5 minutes before expiration)
+            refresh_threshold = timedelta(minutes=5)
+
+            if remaining_time < refresh_threshold:
+                # Generate a new token with an extended expiration time
+                new_token = create_access_token(identity=identity, expires_delta=app.config["JWT_EXPIRATION_DELTA"])
+
+                # Set the new token in the response headers
+                response.headers["X-Refreshed-Token"] = new_token
+
+    except Exception as e:
+        # Handle any exceptions that may occur during token refresh (e.g., invalid token)
+        print(f"Error refreshing token: {str(e)}")
+
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
